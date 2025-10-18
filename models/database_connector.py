f92b7ecb155295e5d00d2edd0553cb44136b155e
Dylan Diaz <dylandiazab@gmail.com>
2025-10-18 19:47:09 -0400

    fix

--- models/database_connector.py
+++ models/database_connector.py
@@ -9,17 +9,17 @@ class Database:
     """
     
     def __init__(self):
-        self.database_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "lopnna_db.sqlite")
+        self.database_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Proyecto_ultima.db")
         self.connection = None
     
     def crearConexion(self):
         """Establece conexión con la base de datos SQLite"""
         try:
             self.connection = sqlite3.connect(self.database_path)
-            print("✅ Conexión a la base de datos SQLite establecida")
+            print("Conexión a la base de datos SQLite establecida")
             return self.connection
         except Error as e:
-            print(f"❌ Error conectando a la base de datos: {e}")
+            print(f"Error conectando a la base de datos: {e}")
             return None
     
     def cerrarConexion(self, conexion=None):
@@ -27,4 +27,4 @@ class Database:
         connection_to_close = conexion or self.connection
         if connection_to_close:
             connection_to_close.close()
-            print("✅ Conexión a la base de datos cerrada")
+            print("Conexión a la base de datos cerrada")
